29847cd047043499b87a3d7041df27b9915eee89
QinJiu <98967953+QinJiu-97@users.noreply.github.com>
2023-02-27 14:55:22 +0800

    Delete myapp directory

--- myapp/src/constants/index.ts
+++
@@ -1,7 +0,0 @@
-export const SYSTEM_LOGO = "https://xingqiu-tuchuang-1256524210.cos.ap-shanghai.myqcloud.com/285/pictureBigLogo.png"
-
-
-/**
- * 琴酒的联系方式
- */
-export const MY_WECHAT = "https://xingqiu-tuchuang-1256524210.cos.ap-shanghai.myqcloud.com/285/picture72a7e7bb3dbd9f78d5db5387639ccfb.jpg"
